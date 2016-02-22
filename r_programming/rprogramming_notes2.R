87b905c75ae729e66f6b6e6b18473d0ab68f8c50
Piergiorgio Calzi <pezzekazil@gmail.com>
2016-02-22 23:12:36 +0000

    adds some notes

---
+++ r_programming/rprogramming_notes2.R
@@ -0,0 +1,16 @@
+library("swirl")
+
+# Control Structure: Introduction
+  # If, else: testing a condition
+  # for: execute a loop a fixed number of times
+  # while: execute a loop while a condition is true
+  # repeat: execute an infinite loop
+  # break: break the execution of a loop
+  # next skip an iteration of a loop
+  # return: exit a function
+
+# Control Structures: if-else
+
+# Swirl 8
+TRUE & c(TRUE, FALSE) # recycle left TRUE
+TRUE && c(TRUE, FALSE) # evaluate only first element vector
