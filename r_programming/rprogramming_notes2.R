f5563bcb9f471cc73663402c631dcface32e1981
Pezzekazil <pezzekazil@gmail.com>
2016-02-25 16:46:01 +0000

    Update rprogramming_notes2.R
    adds for loop

--- r_programming/rprogramming_notes2.R
+++ r_programming/rprogramming_notes2.R
@@ -10,6 +10,35 @@ library("swirl")
   # return: exit a function
 
 # Control Structures: if-else
+  if (x > 3) {
+    y <- 10
+  } else if (x < 2) {
+    y <- 5
+  } else {
+    y <- 0
+  }
+  
+# Control Structures: for-loop
+  for (i in 1:10) {
+    print(i)
+  }
+  
+  x <- c("a", "b", "c", "d")
+  for (i in 1:4) {
+    print(x[i])
+  }
+  #seq_along take the length of the parameter and generate a seqence from 1 to that number
+  for (i in seq_along(x)) { 
+    print (x[i])
+  }
+  for(letter in x) {
+    print(letter)
+  }
+  # for loops can be nested. seq_len() is useful as well as nrow() and ncol().
+  
+# Control Structures: while-loop
+  
+
 
 # Swirl 8
 TRUE & c(TRUE, FALSE) # recycle left TRUE
